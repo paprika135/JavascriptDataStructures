2bee8a9a9f3e37de09076aac8f902ea897241949
weizhu-xiaomi <ffff0808ljc@gmail.com>
2024-06-20 17:45:27 +0800

    利用已构建的二叉搜索树的基础，构建avl树

---
+++ avlTree/src/binaryTree/utils/compare.mjs
@@ -0,0 +1,23 @@
+export const Compare = {
+    LESS_THAN:-1,
+    BIGGER_THAN:1 
+};
+
+export function compare(a,b){
+    if(a < b){
+        return -1;
+    }
+    if (a > b){
+        return 1;
+    }
+    //a等于b
+    return 0;
+}
+
+
+export function defaultCompare(a,b){
+    if(a === b){
+        return 0;
+    }
+    return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
